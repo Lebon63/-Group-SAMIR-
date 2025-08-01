2b5387ff305aeb191288f19e2a2def90fb095262
Lebon63 <129806686+Lebon63@users.noreply.github.com>
2025-08-01 15:16:34 +0100

    Update database.py

--- backend/db/database.py
+++ backend/db/database.py
@@ -3,8 +3,17 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 import os
 
-DATABASE_URL = "postgres://postgres:Eventsnap101%23@db.ylytqdkdulrtducrekvp.supabase.co:5432/postgres?sslmode=require"
+DATABASE_URL = "postgresql+psycopg2://postgres:Eventsnap101%23@db.ylytqdkdulrtducrekvp.supabase.co:5432/postgres"
 
-engine = create_engine(DATABASE_URL, pool_pre_ping=True)
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={
+        "keepalives": 1,
+        "keepalives_idle": 30,
+        "keepalives_interval": 10,
+        "sslmode": "require"  # Explicit SSL requirement
+    }
+)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 Base = declarative_base()
