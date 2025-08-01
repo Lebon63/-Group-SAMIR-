f8478c59522382bf1a2ff704c70c71a8a3327a6f
Lebon63 <129806686+Lebon63@users.noreply.github.com>
2025-08-01 16:28:47 +0100

    Update vite.config.ts

--- Frontend/vite.config.ts
+++ Frontend/vite.config.ts
@@ -1,29 +1,37 @@
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react-swc";
-import path from "path";
-import { componentTagger } from "lovable-tagger";
+import { defineConfig, loadEnv } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import path from 'path';
+import { componentTagger } from 'lovable-tagger';
 
-// https://vitejs.dev/config/
-export default defineConfig(({ mode }) => ({
-  server: {
-  host: "::",
-  port: 8080,
-  proxy: {
-    '/api': {
-      target: import.meta.env.VITE_BACKEND_URL,
-      changeOrigin: true,
-      rewrite: (path) => path.replace(/^\/api/, '')
+export default defineConfig(({ mode }) => {
+  
+  const env = loadEnv(mode, process.cwd(), '');
+
+  return {
+    base: '/',
+    server: {
+      host: "::",
+      port: 8080,
+      proxy: {
+        '/api': {
+          target: env.VITE_BACKEND_URL || 'http://localhost:8000',
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '')
+        }
+      }
+    },
+    plugins: [
+      react(),
+      mode === 'development' && componentTagger(),
+    ].filter(Boolean),
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
+    },
+    
+    define: {
+      'process.env': env
     }
   }
-},
-  plugins: [
-    react(),
-    mode === 'development' &&
-    componentTagger(),
-  ].filter(Boolean),
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
-    },
-  },
-}));
+});
